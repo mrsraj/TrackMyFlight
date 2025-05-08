dd2f617cf764c59c74f094235aa1db19bee0696b
Sachin Raj <sachinraj6392@gmail.com>
2025-05-09 01:24:52 +0530

    Add about Data

---
+++ src/Data/AboutInfo.js
@@ -0,0 +1,42 @@
+const aboutInfo = [
+    {
+        title: "Company Overview",
+        content: "RSR Aviation Services Pvt. Ltd. is a premier provider of aerospace consumables for manufacturing and maintenance in the aerospace and defense sectors. Our focus on quality and reliability positions us as a trusted partner in the industry.",
+        img: "/About/1.png",
+    },
+    {
+        title: "Our Specialization",
+        content: "We specialize in high-quality consumables tailored for the aerospace industry, meeting the rigorous standards of manufacturers and maintenance teams. Our products enhance durability, performance, and safety.",
+        img: "/About/2.png",
+    },
+    {
+        title: "Extensive Product Range",
+        content: "Our inventory includes lubricants, adhesives, fasteners, primers, film adhesives, paints, and electrical supplies, supporting aerospace applications from production to repair.",
+        img: "/About/3.png",
+    },
+    {
+        title: "Warehousing Capabilities",
+        content: "RSR Aviation's warehouse spans 4,000 sq. ft., expanding to 10,000 sq. ft. by Nov 2024. It is equipped for large inventories, including hazardous goods, showing our readiness to scale for aerospace demands.",
+        img: "/About/4.png",
+    },
+    {
+        title: "Cold Storage & Temperature",
+        content: "We have in-house cold storage (1,000 liters) for 6–8 full-size adhesive rolls. Our refrigeration system stores 1,500 liters, planned to double by Nov 2024. We ensure temperature-sensitive materials are kept in optimal conditions.",
+        img: "/About/5.png",
+    },
+    {
+        title: "Vision & Values",
+        content: `
+  1. Global Leadership: Become the leading supplier of aerospace consumables.
+  2. Standard Setting: Set benchmarks for quality, safety, and innovation.
+  3. Industry Impact: Shape the future of aviation through superior products.
+  Quality: Ensure products meet aerospace standards.
+  Safety: Provide reliable, certified consumables.
+  Customer Focus: Offer unmatched service and support.
+  Integrity: Operate with ethics and responsibility.`,
+        img: "/About/6.png",
+    },
+];
+
+
+export default aboutInfo;
